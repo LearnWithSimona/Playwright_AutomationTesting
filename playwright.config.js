4ca6ca7ebde113e83991b49541529ac50fbd4cea
SimonaMladenovska <mladenovskasimona14@gmail.com>
2024-09-22 16:48:44 +0200

    Remove comments

--- playwright.config.js
+++ playwright.config.js
@@ -1,29 +1,14 @@
-// playwright.config.js
 require('dotenv').config();
 
 module.exports = {
-  retries:3,
-    use: {
-      // Base URL for your tests
-      baseURL: 'http://localhost:3000', 
-  
-      // Headless mode for browser testing
-      headless: true, 
-  
-      // Default timeout for tests (in ms)
-      timeout: 30000, 
-  
-      // Browser to use: chromium, firefox, or webkit
-      browserName: 'chromium', 
-  
-      // Default viewport size
-      viewport: { width: 1280, height: 720 },
-  
-      // Take a screenshot on failure
-      screenshot: 'only-on-failure',
-  
-      // Record videos of test executions
-      video: 'retain-on-failure',
-    },
-  };
-  
+  retries: 3,
+  use: {
+    baseURL: 'http://localhost:3000',
+    headless: true,
+    timeout: 30000,
+    browserName: 'chromium',
+    viewport: { width: 1280, height: 720 },
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+};
